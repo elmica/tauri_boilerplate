fe75883d982a58c3b994bef19a02d48483342cf7
elmica <mb.codingtest@gmail.com>
2025-04-19 19:44:23 -0400

    added theme switcher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import ThemeSelect from '$lib/theme-select.svelte';
 </script>
 
-<div class="hero bg-base-200 min-h-screen">
-	<div class="hero-content text-center">
-		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">Yes!!</h1>
+<div>
+	<ThemeSelect />
+
+	<div class="hero bg-base-200 min-h-screen">
+		<div class="hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="text-5xl font-bold">Yes!!</h1>
+			</div>
 		</div>
 	</div>
 </div>
